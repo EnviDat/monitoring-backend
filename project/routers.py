8608f6c7d012ef0213cbda64538a796767c2370e
Rebecca Kurup <rebecca.kurup@wsl.ch>
2020-09-08 08:46:33 +0200

    Updated requirements and router

--- project/routers.py
+++ project/routers.py
@@ -14,7 +14,6 @@ class MonitoringRouter:
 
     def db_for_write(self, model):
         if model._meta.app_label in self.route_app_labels:
-            print("APP LABEL " + model._meta.app_label)
             return model._meta.app_label
         else:
             print('WARNING (routers.py) non-valid model": {0}'.format(model))
