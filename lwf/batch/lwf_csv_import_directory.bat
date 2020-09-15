d9a7f012cc11caf17d1348ebf8eab6df44973bba
Rebecca Kurup <rebecca.kurup@wsl.ch>
2020-09-15 09:32:19 +0200

    Added lwf_csv_import_directory.bat

---
+++ lwf/batch/lwf_csv_import_directory.bat
@@ -0,0 +1,29 @@
+cd "path\to\project\directory"
+
+python manage.py lwf_csv_import -s leb -p LWFMeteo -i lwf/data/lebforest.csv -d lwf/data -m leb -t directory &
+python manage.py lwf_csv_import -s baf -p LWFMeteo -i lwf/data/baffield.csv -d lwf/data -m baf -t directory &
+python manage.py lwf_csv_import -s bab -p LWFMeteo -i lwf/data/babforest.csv -d lwf/data -m bab -t directory &
+python manage.py lwf_csv_import -s clb -p LWFMeteo -i lwf/data/cibforest.csv -d lwf/data -m clb -t directory &
+python manage.py lwf_csv_import -s clf -p LWFMeteo -i lwf/data/clffield.csv -d lwf/data -m clf -t directory &
+python manage.py lwf_csv_import -s jub -p LWFMeteo -i lwf/data/jubforest.csv -d lwf/data -m jub -t directory &
+python manage.py lwf_csv_import -s juf -p LWFMeteo -i lwf/data/juffield.csv -d lwf/data -m juf -t directory &
+python manage.py lwf_csv_import -s isb -p LWFMeteo -i lwf/data/isbforest.csv -d lwf/data -m isb -t directory &
+python manage.py lwf_csv_import -s isf -p LWFMeteo -i lwf/data/isffield.csv -d lwf/data -m isf -t directory &
+python manage.py lwf_csv_import -s btf -p LWFMeteo -i lwf/data/btffield.csv -d lwf/data -m btf -t directory &
+python manage.py lwf_csv_import -s btb -p LWFMeteo -i lwf/data/btbforest.csv -d lwf/data -m btb -t directory &
+python manage.py lwf_csv_import -s nab -p LWFMeteo -i lwf/data/nabforest.csv -d lwf/data -m nab -t directory &
+python manage.py lwf_csv_import -s naf -p LWFMeteo -i lwf/data/naffield.csv -d lwf/data -m naf -t directory &
+python manage.py lwf_csv_import -s vsb -p LWFMeteo -i lwf/data/vsbforest.csv -d lwf/data -m vsb -t directory &
+python manage.py lwf_csv_import -s vsf -p LWFMeteo -i lwf/data/vsffield.csv -d lwf/data -m vsf -t directory &
+python manage.py lwf_csv_import -s lab -p LWFMeteo -i lwf/data/labforest.csv -d lwf/data -m lab -t directory &
+python manage.py lwf_csv_import -s laf -p LWFMeteo -i lwf/data/laffield.csv -d lwf/data -m laf -t directory &
+python manage.py lwf_csv_import -s vob -p LWFMeteo -i lwf/data/vobforest.csv -d lwf/data -m vob -t directory &
+python manage.py lwf_csv_import -s vof -p LWFMeteo -i lwf/data/voffield.csv  -d lwf/data -m vof -t directory &
+python manage.py lwf_csv_import -s neb -p LWFMeteo -i lwf/data/nebforest.csv  -d lwf/data -m neb -t directory &
+python manage.py lwf_csv_import -s nef -p LWFMeteo -i lwf/data/neffield.csv  -d lwf/data -m nef -t directory &
+python manage.py lwf_csv_import -s nob -p LWFMeteo -i lwf/data/nobforest.csv  -d lwf/data -m nob -t directory &
+python manage.py lwf_csv_import -s nof -p LWFMeteo -i lwf/data/noffield.csv  -d lwf/data -m nof -t directory &
+python manage.py lwf_csv_import -s otb -p LWFMeteo -i lwf/data/otbforest.csv  -d lwf/data -m otb -t directory &
+python manage.py lwf_csv_import -s otf -p LWFMeteo -i lwf/data/otffield.csv  -d lwf/data -m otf -t directory &
+python manage.py lwf_csv_import -s scb -p LWFMeteo -i lwf/data/scbforest.csv  -d lwf/data -m scb -t directory &
+python manage.py lwf_csv_import -s scf -p LWFMeteo -i lwf/data/scffield.csv  -d lwf/data -m scf -t directory &
