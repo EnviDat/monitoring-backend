d0407eca95571fc890b27043afa4816621acf3be
Rebecca Kurup <rebecca.kurup@wsl.ch>
2020-09-15 17:35:39 +0200

    Added arguments to gcnet_csv_export.py

--- gcnet/management/commands/gcnet_csv_export.py
+++ gcnet/management/commands/gcnet_csv_export.py
@@ -1,20 +1,12 @@
 # Example command:
-#   python manage.py gcnet_csv_export -s 01_swisscamp -c config/stations.ini -i gcnet/data/1.csv -d gcnet/data -m swisscamp_01d -t web
+#   python manage.py gcnet_csv_export -d gcnet/output -n 0_swisscamp -m swisscamp_01d
 
-from pathlib import Path
-import requests
-from django.core.management.base import BaseCommand
-from postgres_copy import CopyMapping
 import importlib
-from django.utils.timezone import make_aware
-
-from gcnet.helpers import quarter_day, half_day, year_day, year_week, gcnet_utc_timestamp, gcnet_utc_datetime
+from pathlib import Path
+from django.core.management.base import BaseCommand
 
 # Setup logging
 import logging
-
-from gcnet.models import swisscamp_01d
-
 logging.basicConfig(filename=Path('gcnet/logs/gcnet_csv_export.log'), format='%(asctime)s   %(filename)s: %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
@@ -23,52 +15,40 @@ logger.setLevel(logging.DEBUG)
 
 class Command(BaseCommand):
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument(
-    #         '-s',
-    #         '--station',
-    #         required=True,
-    #         help='Station number and name, for example "02_crawford"'
-    #     )
-    #
-    #     parser.add_argument(
-    #         '-c',
-    #         '--config',
-    #         required=True,
-    #         help='Path to stations config file'
-    #     )
-    #
-    #     parser.add_argument(
-    #         '-i',
-    #         '--inputfile',
-    #         required=True,
-    #         help='Path or URL to input csv file'
-    #     )
-    #
-    #     parser.add_argument(
-    #         '-d',
-    #         '--directory',
-    #         required=True,
-    #         help='Path to directory which will contain intermediate processing csv file'
-    #     )
-    #
-    #     parser.add_argument(
-    #         '-m',
-    #         '--model',
-    #         required=True,
-    #         help='Django Model to map data import to'
-    #     )
-    #
-    #     parser.add_argument(
-    #         '-t',
-    #         '--typesource',
-    #         required=True,
-    #         help='Type of data source. Valid options are a file path: "directory" or a url: "web"'
-    #     )
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-d',
+            '--directory',
+            required=True,
+            help='Path to directory which will contain output csv file'
+        )
+
+        parser.add_argument(
+            '-n',
+            '--name',
+            required=True,
+            help='Name for csv file, for example "0_swisscamp"'
+        )
+
+        parser.add_argument(
+            '-m',
+            '--model',
+            required=True,
+            help='Django Model to export data from'
+        )
 
     def handle(self, *args, **kwargs):
 
-        swisscamp_01d.objects.order_by('timestamp_iso').to_csv('gcnet/output/0_swisscamp.csv',
+        # Create output_path from arguments
+        output_path = Path(kwargs['directory'] + '/' + kwargs['name'] + '.csv')
+
+        # Get the model
+        class_name = kwargs['model'].rsplit('.', 1)[-1]
+        package = importlib.import_module("gcnet.models")
+        model_class = getattr(package, class_name)
+
+        # Export database table to csv with only 'timestamp_iso' and parameter fields
+        model_class.objects.order_by('timestamp_iso').to_csv(output_path,
                                                                'timestamp_iso',
                                                                'swin',
                                                                'swout',
@@ -100,3 +80,6 @@ class Command(BaseCommand):
                                                                'windspeed_u2_stdev',
                                                                'reftemp'
                                                                )
+
+        # Log import message
+        logger.info('{0} successfully exported, written in {1}'.format(model_class, output_path))
