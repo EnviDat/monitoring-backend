6a1ef560c3d855b47dc9a137701ff72d0f4d0439
Rebecca Kurup <rebecca.kurup@wsl.ch>
2020-10-27 16:02:55 +0100

    Removed unused imports

--- gcnet/urls.py
+++ gcnet/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.urls import path, re_path
+from django.urls import path
 
 from gcnet.views import get_dynamic_data, get_derived_data, get_model_stations, streaming_csv_view_v1
 
