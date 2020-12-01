e5c3812ce6ba51f5412e82e45bcebcc027ad7dbc
Rebecca Kurup <rebecca.kurup@wsl.ch>
2020-12-01 15:17:23 +0100

    Worked on write_nead.py

--- gcnet/util/write_nead.py
+++ gcnet/util/write_nead.py
@@ -1,6 +1,7 @@
+import pandas as pd
 import argparse
 
-from gcnet.util.helpers import get_null_value, get_model
+from gcnet.util.helpers import get_model, get_hashed_lines
 from gcnet.util.write_nead_config import write_nead_config
 
 
@@ -26,22 +27,25 @@ def write_nead(args=None):
     nead_config = 'gcnet/config/nead_header.ini'
 
     # Assign station_model
-    station_model = args.station
+    station_model = args.station.strip()
 
     # Assign NEAD output csv
-    output_nead = station_model + '.csv'
+    output_nead = 'gcnet/output/nead/' + station_model + '.csv'
 
     # # Assign null_value
     # null_value = get_null_value(kwargs['nodata'])
+    # TODO fix this
+    null_value = '-999'
 
     # # Assign timestamp_meaning
     # timestamp_meaning = kwargs['timestamp_meaning']
+    # TODO fix this
+    timestamp_meaning = 'end'
 
-    # ================================  VALIDATE VARIABLES =========================================================
-    # Get and validate the model_class
+    # ================================  VALIDATE STATION MODEL =======================================================
+    # Validate station_model
     try:
-        model_class = get_model(station_model.strip())
-        print(station_model + 'VALIDATED')
+        model_class = get_model(station_model)
     except Exception as e:
         raise Exception("{1}\nWARNING (write_nead.py): Invalid station:{0}".format(station_model, e))
 
@@ -60,32 +64,33 @@ def write_nead(args=None):
     #                                     "<h3>Start and end dates should both be in ISO timestamp "
     #                                     "date format: YYYY-MM-DD ('2019-12-04')</h3>")
     #
-    # # =============================== PROCESS NEAD HEADER ===========================================================
+
+    # =============================== PROCESS NEAD HEADER ===========================================================
     # Get NEAD header
-    # config_buffer, nead_config_parser = write_nead_config(config_path=nead_config, model=station_model,
-    #                                                       stringnull=null_value, delimiter=',',
-    #                                                       ts_meaning=timestamp_meaning)
-    # #
-    # # Check if config_buffer or nead_config_parser are None
-    # if nead_config_parser is None or config_buffer is None:
-    #     return HttpResponseNotFound("<h1>Page not found</h1>"
-    #                                 "<h3>Check that valid 'model' (station) entered in URL: {0}</h3>"
-    #                                 .format(kwargs['model']))
-    #
-    # # Fill hash_lines with config_buffer lines prepended with '# '
-    # hash_lines = get_hashed_lines(config_buffer)
-    #
-    # # Assign display_values from database_fields in nead_config_parser
-    # database_fields = nead_config_parser.get('FIELDS', 'database_fields')
-    # display_values = list(database_fields.split(','))
-    #
-    # # ===================================  STREAM NEAD DATA ===========================================================
-    # # Create the streaming response object and output csv
-    # response = StreamingHttpResponse(stream(version, hash_lines, model_class, display_values, timestamp_meaning,
-    #                                         null_value, start, end, dict_fields={}), content_type='text/csv')
-    # response['Content-Disposition'] = 'attachment; filename=' + output_csv
-    #
-    # return response
+    config_buffer, nead_config_parser = write_nead_config(config_path=nead_config, model=station_model,
+                                                          stringnull=null_value, delimiter=',',
+                                                          ts_meaning=timestamp_meaning)
+
+    # Fill hash_lines with config_buffer lines prepended with '# '
+    hash_lines = get_hashed_lines(config_buffer)
+
+    # ===================================  WRITE NEAD ===========================================================
+    # TODO fix this to read Pandas data frame data
+    # TODO determine if data need to be processed: null values changed
+    data_df = pd.read_csv('test_data.csv')
+    # # Read csv data
+    # with open('test_data.csv', 'r') as nead_data:
+    #     data = nead_data.read()
+
+    # Write nead_header into NEAD file
+    with open(output_nead, 'w', newline='\n') as nead_header:
+        for row in hash_lines:
+            nead_header.write(row)
+
+    # Append data to header
+    with open(output_nead, 'a') as nead:
+        #nead.write(data)
+        data_df.to_csv(nead, index=False, line_terminator='\n')
 
 
 if __name__ == '__write_nead__':
