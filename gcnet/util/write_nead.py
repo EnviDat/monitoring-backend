3ed1a70eebe244119650ed0721e27f41d75681aa
Rebecca Kurup <rebecca.kurup@wsl.ch>
2020-12-03 13:56:40 +0100

    Improved write_nead.py

--- gcnet/util/write_nead.py
+++ gcnet/util/write_nead.py
@@ -1,7 +1,6 @@
 from io import StringIO
 import configparser
 from pathlib import Path
-import pandas as pd
 
 
 def read_config(config_path: str):
@@ -18,7 +17,7 @@ def read_config(config_path: str):
     return config
 
 
-# Fill hash_lines with config lines prepended with '# '
+# Assign hash_lines with config lines prepended with '# '
 def get_hashed_lines(config):
     hash_lines = []
     for line in config.replace('\r\n', '\n').split('\n'):
@@ -27,21 +26,33 @@ def get_hashed_lines(config):
     return hash_lines
 
 
-def write_nead(data_frame, nead_header, output_name):
+# Writes NEAD file (CSV file with NEAD formatted header)
+# Columns written in NEAD output will be the fields designated in the
+#   NEAD configuration file setting 'fields' in section 'FIELDS'
+# Paths must be absolute
+# Paths can use 'r' in front of strings with paths to designate them as a raw string if using Windows
+#   style paths with backslashes
+# Arguments:
+#   data_frame    Pandas dataframe
+#   nead_config   string with Path to NEAD configuration file, ex. r'C:\Users\Icy\gcnet\config\nead_header.ini'
+#   output_path   string with Path assigned to output NEAD file,
+#                 be sure to designate name for new NEAD file at end of string
+#                 ex.  r'C:\Icy\gcnet\output\nead\my_nead_file_name')
+def write_nead(data_frame, nead_config, output_path):
 
-    # Assign nead_output to output_name with .csv extension
-    nead_output = '{0}.csv'.format(output_name)
+    # Assign nead_output to output_path with .csv extension
+    nead_output = Path('{0}.csv'.format(output_path))
 
-    # Read nead_header into nead_config
-    nead_config = read_config(nead_header)
+    # Read nead_config into conf
+    conf = read_config(Path(nead_config))
 
-    # Assign fields from nead_header 'fields'
-    fields = nead_config.get('FIELDS', 'fields')
+    # Assign fields from nead_config 'fields', convert to list in fields_list
+    fields =conf.get('FIELDS', 'fields')
     fields_list = fields.split(',')
 
-    # Dynamically write NEAD header into buffer
+    # Write conf into buffer
     buffer = StringIO()
-    nead_config.write(buffer)
+    conf.write(buffer)
 
     # Assign hash_lines with nead_config lines prepended with '# '
     hash_lines = get_hashed_lines(buffer.getvalue())
@@ -51,12 +62,6 @@ def write_nead(data_frame, nead_header, output_name):
         for row in hash_lines:
             nead_header.write(row)
 
-    # Append data to header
+    # Append data to header, omit indices, omit dataframe header, and output columns in fields_list
     with open(nead_output, 'a') as nead:
         data_frame.to_csv(nead, index=False, header=False, columns=fields_list, line_terminator='\n')
-
-
-# =============================== TESTS =====================================================================
-data_df = pd.read_csv('test_data.csv')
-write_nead(data_df, 'gcnet/config/nead_header.ini', 'test_name')
-
