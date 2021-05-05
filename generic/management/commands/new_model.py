9da7f8a3ecca949c137145155e9bb8f0a8401191
Rebecca Kurup <rebecca.kurup@wsl.ch>
2021-05-05 11:16:44 +0200

    Wrote validators for generic new_model

--- generic/management/commands/new_model.py
+++ generic/management/commands/new_model.py
@@ -8,9 +8,9 @@ from django.core.management.base import BaseCommand
 __version__ = '0.0.1'
 __author__ = u'Rebecca Buchholz'
 
-from generic.util.commands_helpers import has_spaces
+from generic.util.commands_helpers import has_spaces, model_exists
 from generic.util.nead import read_config
-from lwf.util.new_model_helpers import execute_commands, model_exists
+from lwf.util.new_model_helpers import execute_commands
 
 # Setup logging
 import logging
@@ -42,6 +42,13 @@ class Command(BaseCommand):
         parent_class = conf.get('configuration', 'parent_class')
         name = conf.get('configuration', 'name')
 
+        # Get app_label of parent_class
+        try:
+            app = self.get_app(parent_class)
+        except Exception as e:
+            logger.error(e)
+            return
+
         # Check if 'database_table_name' in config file is in valid format (lowercase and no spaces)
         database_table_name = conf.get('configuration', 'database_table_name')
         if has_spaces(database_table_name):
@@ -52,28 +59,32 @@ class Command(BaseCommand):
             return
 
         # Create models file path string
-        model_path = self.get_model_path(parent_class)
+        try:
+            model_path = self.get_model_path(parent_class)
+        except Exception as e:
+            logger.error(e)
+            return
 
         # Set table_exists to False
         table_exists = False
 
         # Check if model ('database_table_name' in config) already exists in corresponding models file
-        try:
-            # First check if model is written in corresponding models file:
-            with open(model_path, 'r') as f:
-                if database_table_name in f.read():
-                    table_exists = True
-                    logger.error(f'ERROR table {database_table_name} already written in {model_path}')
-                    return
-        except FileNotFoundError as e:
-            logger.error(f'ERROR file not found {model_path}, exception {e}')
-
-        # Check if table already exists in database
-        # long_db_name = 'lwf_{0}'.format(database_table_name)
-
-        if model_exists(database_table_name):
-            table_exists = True
-            # print('WARNING (lwf_new_model.py): Table {0} already exists in database'.format(long_db_name))
+        # try:
+        #     # First check if model is written in corresponding models file:
+        #     with open(model_path, 'r') as f:
+        #         if database_table_name in f.read():
+        #             table_exists = True
+        #             logger.error(f'ERROR table {database_table_name} already written in {model_path}')
+        #             return
+        # except FileNotFoundError as e:
+        #     logger.error(f'ERROR file not found {model_path}, exception {e}')
+        #
+        # # Check if table already exists in database
+        # # long_db_name = 'lwf_{0}'.format(database_table_name)
+        #
+        # if model_exists(database_table_name):
+        #     table_exists = True
+        # print('WARNING (lwf_new_model.py): Table {0} already exists in database'.format(long_db_name))
 
         # # If child class does not exist in corresponding models file or database
         # # write it to corresponding models file and run migrations to add it to database
@@ -116,5 +127,17 @@ class Command(BaseCommand):
             return f'gcnet/models.py'
 
         else:
-            logger.error(f'ERROR {parent_class} parent class does not exist or does not have model_path specified')
-            return
+            raise Exception(f'ERROR {parent_class} parent class does not exist or does not have model_path specified')
+
+    # Check which kind of app should be used
+    @staticmethod
+    def get_app(parent_class):
+
+        if parent_class in ['LWFMeteo', 'LWFStation']:
+            return 'lwf'
+
+        elif parent_class in ['Station']:
+            return 'gcnet'
+
+        else:
+            raise Exception(f'ERROR {parent_class} parent class does not exist or does not have app specified')
--- generic/util/commands_helpers.py
+++ generic/util/commands_helpers.py
@@ -1,8 +1,23 @@
+from django.apps import apps
 
+
+# ----------------------------------------  New Model Helpers ---------------------------------------------------------
 
 # Returns True if string has spaces
 def has_spaces(string):
     if ' ' in string:
+        return True
+    else:
+        return False
+
+
+def model_exists(table_name):
+
+    models = apps.all_models['lwf']
+    model_names = list(models.keys())
+    print(model_names)
+
+    if table_name in model_names:
         return True
     else:
         return False
