1e43c1cec9f47e636ad9bf8a96910cf3a434c8c4
Rebecca Kurup <rebecca.kurup@wsl.ch>
2021-05-07 15:44:23 +0200

    Added lwf_new_model batch file

---
+++ lwf/batch/lwf_new_model.bat
@@ -0,0 +1,33 @@
+cd "path\to\project\"
+CALL .\<venv name>\Scripts\activate.bat
+
+python manage.py new_model -c lwf/config/alpthal_bestand_1.ini &
+python manage.py new_model -c lwf/config/beatenberg_bestand_2.ini &
+python manage.py new_model -c lwf/config/beatenberg_freiland_3.ini &
+python manage.py new_model -c lwf/config/bettlachstock_bestand_4.ini &
+python manage.py new_model -c lwf/config/bettlachstock_freiland_5.ini &
+python manage.py new_model -c lwf/config/celerina_bestand_6.ini &
+python manage.py new_model -c lwf/config/celerina_freiland_7.ini &
+python manage.py new_model -c lwf/config/chironico_bestand_8.ini &
+python manage.py new_model -c lwf/config/chironico_freiland_9.ini &
+python manage.py new_model -c lwf/config/isone_bestand_12.ini &
+python manage.py new_model -c lwf/config/isone_freiland_13.ini &
+python manage.py new_model -c lwf/config/jussy_bestand_14.ini &
+python manage.py new_model -c lwf/config/jussy_freiland_15.ini &
+python manage.py new_model -c lwf/config/lausanne_bestand_18.ini &
+python manage.py new_model -c lwf/config/lausanne_freiland_19.ini &
+python manage.py new_model -c lwf/config/lens_bestand_20.ini &
+python manage.py new_model -c lwf/config/nationalpark_bestand_22.ini &
+python manage.py new_model -c lwf/config/nationalpark_freiland_23.ini &
+python manage.py new_model -c lwf/config/neunkirch_bestand_24.ini &
+python manage.py new_model -c lwf/config/neunkirch_freiland_25.ini &
+python manage.py new_model -c lwf/config/novaggio_bestand_26.ini &
+python manage.py new_model -c lwf/config/novaggio_freiland_27.ini &
+python manage.py new_model -c lwf/config/othmarsingen_bestand_28.ini &
+python manage.py new_model -c lwf/config/schaenis_bestand_31.ini &
+python manage.py new_model -c lwf/config/schaenis_freiland_32.ini &
+python manage.py new_model -c lwf/config/visp_bestand_33.ini &
+python manage.py new_model -c lwf/config/visp_freiland_34.ini &
+python manage.py new_model -c lwf/config/vordemwald_bestand_35.ini &
+python manage.py new_model -c lwf/config/vordemwald_freiland_36.ini &
+pause
