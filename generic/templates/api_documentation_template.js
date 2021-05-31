7d1425e8bd229c83fb213ccc1fa7f07ab8321125
Rebecca Kurup <rebecca.kurup@wsl.ch>
2021-05-31 18:16:38 +0200

    Updated generic API documentaiton

--- generic/templates/api_documentation_template.js
+++ generic/templates/api_documentation_template.js
@@ -1,23 +1,32 @@
 // ************************************ Assign JSON path and JSON keys ***********************************************
 
-let json_url = './api_documentation_template.json';
+const json_url = './api_documentation_template.json';
 
-let json_keys = ['page_title', 'api_title', 'into_text', 'nodata', 'app', 'model', 'parameter', 'parameter_several'];
+// TODO add new keys
+const json_keys = ['page_title', 'api_title', 'into_text', 'nodata', 'app', 'model', 'parameter', 'parameter_several',
+                    'start_date'];
 
 
-// -------------------------------  Call loadJson() passing JSON URL as an argument ----------------------------------
+// -------------------------------  Call injectJson() passing JSON URL as an argument ----------------------------------
 
-loadJson(json_url);
+injectJson(json_url);
 
 
 // ------------------------------- Function used to validate JSON file has required keys-------------------------------
 
-// Throws error if a key from keys does not exist in json_object, else returns true
+// Throws error if one or more keys from 'keys' array does not exist in json_object, else returns true
 function jsonKeysValid(json_object, keys) {
+
+    let missingKeys = [];
+
     for (let i=0; i < keys.length; i++) {
         if (!json_object.hasOwnProperty(keys[i])) {
-            throw new Error('JSON file missing key: ' + keys[i]);
+            missingKeys.push(keys[i]);
         }
+    }
+
+    if (missingKeys.length > 0) {
+        throw new Error('JSON file missing key(s): ' + missingKeys);
     }
     return true;
 }
@@ -25,16 +34,76 @@ function jsonKeysValid(json_object, keys) {
 
 // ------------------------------- Function used to assign innerHTML of element arrays --------------------------------
 
-function assignElements (elementsArray, elementString) {
+function assignElementsArray (elementsArray, elementString) {
    for (let i=0; i < elementsArray.length; i++) {
        elementsArray[i].innerHTML = elementString;
    }
 }
 
 
-// ----------------------------- Function to get JSON Data and fill html template elements with JSON values -----------
+// ------------------------------- Function used to assign innerHTML of elements --------------------------------------
 
-function loadJson(url) {
+function assignElements (data) {
+
+    // ----------------------------- Template variables used once --------------------------------------------
+    document.getElementById("page_title").innerHTML = data.page_title;
+    document.getElementById("api_title").innerHTML = data.api_title;
+    document.getElementById("intro_text").innerHTML = data.into_text;
+
+    // ------------------------------- Template variables used multiple times --------------------------------
+    let appElements = document.querySelectorAll("span.app_name");
+    assignElementsArray(appElements, data.app);
+
+    let modelElements = document.querySelectorAll("span.model_name");
+    assignElementsArray(modelElements, data.model);
+
+    let nodataElements = document.querySelectorAll("span.nodata_name");
+    assignElementsArray(nodataElements, data.nodata);
+
+    let parameterElements = document.querySelectorAll("span.parameter_name");
+    assignElementsArray(parameterElements, data.parameter);
+
+    let parameterSeveralElements = document.querySelectorAll("span.parameter_several");
+    assignElementsArray(parameterSeveralElements, data.parameter_several);
+
+    // ----------------------------- URL template variables --------------------------------------------------
+    // TODO use querySelectorAll to fix URL href links in HTML
+    let urlModelsElements = document.querySelectorAll("span.url_models");
+    assignElementsArray(urlModelsElements, `${data.api_host}/${data.app}/models/`);
+
+    let urlNeadElements = document.querySelectorAll(".url_nead");
+    document.getElementById("test_link").setAttribute("href", `https://www.envidat.ch/data-api/${data.app}/nead/${data.model}/end/empty/${data.start_date}/${data.end_date}`)
+    assignElementsArray(urlNeadElements, `https://www.envidat.ch/data-api/${data.app}/nead/${data.model}/end/empty/${data.start_date}/${data.end_date}`);
+
+    // document.getElementById("url_nead").innerHTML = `https://www.envidat.ch/data-api/${data.app}/nead/${data.model}/end/empty/${data.start_date}/${data.end_date}`;
+
+    document.getElementById("url_nead_all").innerHTML =  `https://www.envidat.ch/data-api/${data.app}/nead/${data.model}/end/empty/`;
+
+    document.getElementById("url_csv").innerHTML = `https://www.envidat.ch/data-api/${data.app}/csv/${data.model}/${data.parameter}/end/-999/${data.start_date}/${data.end_date}/`;
+
+    document.getElementById("url_csv_several").innerHTML = `https://www.envidat.ch/data-api/${data.app}/csv/${data.model}/${data.parameter_several}/end/-999/${data.start_date}/${data.end_date}/`;
+
+    document.getElementById("url_json").innerHTML = `https://www.envidat.ch/data-api/${data.app}/json/${data.model}/${data.parameter}/2020-11-04T17:00:00/2020-11-10T00:00:00/`;
+
+    document.getElementById("url_json_several").innerHTML = `https://www.envidat.ch/data-api/${data.app}/json/${data.model}/${data.parameter_several}/2020-11-04T17:00:00/2020-11-10T00:00:00/`;
+
+    document.getElementById("url_daily_json").innerHTML = `http://www.envidat.ch/data-api/${data.app}/summary/daily/json/${data.model}/${data.parameter}/${data.start_date}/${data.end_date}/`;
+
+    document.getElementById("url_daily_json_several").innerHTML = `http://www.envidat.ch/data-api/${data.app}/summary/daily/json/${data.model}/${data.parameter_several}/${data.start_date}/${data.end_date}/`;
+
+    document.getElementById("url_daily_csv").innerHTML = `https://www.envidat.ch/data-api/${data.app}/summary/daily/csv/${data.model}/${data.parameter}/end/empty/${data.start_date}/${data.end_date}/`;
+
+    document.getElementById("url_daily_csv_several").innerHTML = `https://www.envidat.ch/data-api/${data.app}/summary/daily/csv/${data.model}/${data.parameter_several}/end/empty/${data.start_date}/${data.end_date}/`;
+
+    document.getElementById("url_metadata").innerHTML = `http://www.envidat.ch/data-api/${data.app}/metadata/${data.model}/${data.parameter}/`;
+
+    document.getElementById("url_metadata_several").innerHTML = `http://www.envidat.ch/data-api/${data.app}/metadata/${data.model}/${data.parameter_several}/`;
+}
+
+
+// --------- Function to get JSON Data, validate JSON keys and fill html template elements with JSON values -----------
+
+function injectJson(url) {
 
     let request = new XMLHttpRequest();
     request.open("GET", url, true);
@@ -44,67 +113,21 @@ function loadJson(url) {
 
         let data;
 
+        // Load and parse JSON file
         if (this.readyState === this.DONE) {
 
             data = JSON.parse(this.response);
 
-            // Validate json file has required keys
+            // Validate JSON file has required keys
             try {
                 jsonKeysValid(data, json_keys);
             }
             catch (err) {
                 alert(err);
-                return;
             }
 
-            // ----------------------------- Template variables used once --------------------------------------------
-            document.getElementById("page_title").innerHTML = data.page_title;
-            document.getElementById("api_title").innerHTML = data.api_title;
-            document.getElementById("intro_text").innerHTML = data.into_text;
-
-            // ------------------------------- Template variables used multiple times --------------------------------
-            let appElements = document.querySelectorAll("span.app_name");
-            assignElements(appElements, data.app);
-
-            let modelElements = document.querySelectorAll("span.model_name");
-            assignElements(modelElements, data.model);
-
-            let nodataElements = document.querySelectorAll("span.nodata_name");
-            assignElements(nodataElements, data.nodata);
-
-            let parameterElements = document.querySelectorAll("span.parameter_name");
-            assignElements(parameterElements, data.parameter);
-
-            let parameterSeveralElements = document.querySelectorAll("span.parameter_several");
-            assignElements(parameterSeveralElements, data.parameter_several);
-
-            // ----------------------------- URL template variables --------------------------------------------------
-            let urlModelsElements = document.querySelectorAll("span.url_models");
-            assignElements(urlModelsElements, `https://www.envidat.ch/data-api/${data.app}/models/`);
-
-            document.getElementById("url_nead").innerHTML = `https://www.envidat.ch/data-api/${data.app}/nead/${data.model}/end/empty/2020-11-03/2020-11-06`;
-
-            document.getElementById("url_nead_all").innerHTML =  `https://www.envidat.ch/data-api/${data.app}/nead/${data.model}/end/empty/`;
-
-            document.getElementById("url_csv").innerHTML = `https://www.envidat.ch/data-api/${data.app}/csv/${data.model}/${data.parameter}/end/-999/2020-11-04/2020-11-06/`;
-
-            document.getElementById("url_csv_several").innerHTML = `https://www.envidat.ch/data-api/${data.app}/csv/${data.model}/${data.parameter_several}/end/-999/2020-11-04/2020-11-06/`;
-
-            document.getElementById("url_json").innerHTML = `https://www.envidat.ch/data-api/${data.app}/json/${data.model}/${data.parameter}/2020-11-04T17:00:00/2020-11-10T00:00:00/`;
-
-            document.getElementById("url_json_several").innerHTML = `https://www.envidat.ch/data-api/${data.app}/json/${data.model}/${data.parameter_several}/2020-11-04T17:00:00/2020-11-10T00:00:00/`;
-
-            document.getElementById("url_daily_json").innerHTML = `http://www.envidat.ch/data-api/${data.app}/summary/daily/json/${data.model}/${data.parameter}/2020-11-03/2020-11-06/`;
-
-            document.getElementById("url_daily_json_several").innerHTML = `http://www.envidat.ch/data-api/${data.app}/summary/daily/json/${data.model}/${data.parameter_several}/2020-11-03/2020-11-06/`;
-
-            document.getElementById("url_daily_csv").innerHTML = `https://www.envidat.ch/data-api/${data.app}/summary/daily/csv/${data.model}/${data.parameter}/end/empty/2020-11-01/2020-11-06/`;
-
-            document.getElementById("url_daily_csv_several").innerHTML = `https://www.envidat.ch/data-api/${data.app}/summary/daily/csv/${data.model}/${data.parameter_several}/end/empty/2020-11-01/2020-11-06/`;
-
-            document.getElementById("url_metadata").innerHTML = `http://www.envidat.ch/data-api/${data.app}/metadata/${data.model}/${data.parameter}/`;
-
-            document.getElementById("url_metadata_several").innerHTML = `http://www.envidat.ch/data-api/${data.app}/metadata/${data.model}/${data.parameter_several}/`;
+            // Assign HTML elements
+            assignElements(data);
 
         }
     });
