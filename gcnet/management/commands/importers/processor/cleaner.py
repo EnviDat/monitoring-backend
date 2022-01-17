a98c03e1a100fb3231f6d03e0bbba82317167dbb
Rebecca Kurup <rebecca.kurup@wsl.ch>
2022-01-17 14:06:29 +0100

    Started refactoring GoesCleaner

--- gcnet/management/commands/importers/processor/cleaner.py
+++ gcnet/management/commands/importers/processor/cleaner.py
@@ -316,7 +316,7 @@ class ArgosCleaner(Cleaner):
                     logger.warning("\t{0} Station  #{1} does not have usable data".format(self.station_type, snum))
 
 
-# TEST refactoring of ArgosCleaner()
+# TODO review and implement ArgosCleanerV2
 class ArgosCleanerV2(Cleaner):
 
     def __init__(self, init_file_path: str, writer: Writer):
@@ -461,6 +461,7 @@ class ArgosCleanerV2(Cleaner):
 
                         # Loop through records
                         for j in range(num_records):
+
                             # Find second table parts occurring after associated first part
                             table_2_current_indices = np.argwhere(
                                 station_data[table_1_indices[j]:, 0] != station_data[table_1_indices[j]:, 9])
@@ -932,6 +933,245 @@ class GoesCleaner(Cleaner):
                 else:  # if no data in adata still output empty wdata array so empty file is created
                     wdata = np.array([])
                     logger.warning("\t{0} Station  #{1} does not have usable data".format(self.station_type, snum))
+
+
+class GoesCleanerV2(Cleaner):
+
+    def __init__(self, init_file_path: str, writer: Writer):
+        Cleaner.__init__(self, init_file_path, "GOES", writer)
+
+    def clean(self, input_data: np.ndarray):
+
+        # Iterate through each station and write json and csv file
+        for section in self.stations_config.sections():
+
+            # Assign station config values
+            station_type = self.stations_config.get(section, "type")
+            is_active = self.stations_config.get(section, "active")
+
+            # Assign constant for column index in input numpy array
+            INPUT_STATION_NUM_COL = 1
+
+            # Process Goes stations
+            if station_type == 'goes' and is_active == 'True':
+
+                # Assign station ID
+                station_id = section
+
+                # Assign station number
+                station_num = int(self.stations_config.get(section, "station_num"))
+
+                logger.info(f'GoesCleaner: Processing {self.station_type} Station #{station_num}...')
+
+                if input_data.size > 0:
+
+                    # Assign station_data to data associated with each station
+                    station_data = np.array(input_data[input_data[:, INPUT_STATION_NUM_COL] == station_num, :])
+
+                    logger.info(f'GoesCleaner: Data size {station_data.size} for Station #{station_num}...')
+                    # if station_data.size <= 0:
+                    #     logger.warning(f'Skipping cleaning of {station_data.size} '
+                    #                    f'for Station #{station_num}, NO DATA')
+                    # continue
+
+                    # Assign unique_array unique rows after ***column 5
+                    # Assign unique_indices to indices of unique rows after
+                    # ***column 5 because data may repeat with different time signature
+                    # TODO assign constant to 5 in line below
+                    unique_array, unique_indices = np.unique(station_data[:, 5:], axis=0, return_index=True)
+                    station_data = station_data[np.sort(unique_indices), :]
+
+                    # Assign station_array to station_data filtered for realistic years and Julian days
+                    # TODO refactor the rest of GoesCleanerV2
+                    station_array = station_data[
+                            (station_data[:, 2] > 1990) & (station_data[:, 2] < 2050) & (station_data[:, 3] >= 0) & (station_data[:, 3] < 367),
+                            :]  # filter realistic time
+                    # #(positive and less than 367 JD, leap year will have 366 days)
+                    # # and sensible year
+
+                    logger.info("GoesCleaner: Clean data size {0} for Station #{1}...".format(station_array.size, station_num))
+                    # if station_array.size <= 0:
+                    #     logger.warning("Skipping cleaning of {0} for Station #{1}, NO DATA after cleaning"
+                    #                    .format(station_array.size, station_num))
+                    #     continue
+
+                    if station_num == 0:  # if 10 meter tower
+                        gdata = np.ones((np.size(station_array, 0), 43)) * 999
+                        gind = np.concatenate((np.arange(0, 7), np.arange(8, 20), np.array([30, 34, 35, 38, 39])))
+                        aind = np.concatenate((np.arange(1, 8), np.arange(8, 20), np.array([24]), np.arange(20, 24)))
+                        gdata[:, gind] = station_array[:, aind]  # format is
+                    elif station_num == 24:  # if East GRIP
+                        gdata = np.ones((np.size(station_array, 0), 43)) * 999
+                        gind = np.concatenate((np.arange(0, 7), np.arange(8, 20), np.array([30]), np.arange(34, 43)))
+                        aind = np.concatenate((np.arange(1, 8), np.arange(8, 20), np.array([31]), np.arange(22, 31)))
+                        gdata[:, gind] = station_array[:, aind]  # format is
+                        # different for this station: no snow temp, only 2 radiation
+                        # statistic measurements
+                    elif station_num == 30:  # if PE Blue
+                        gdata = np.ones((np.size(station_array, 0), 43)) * 999
+                        gind = np.concatenate((np.arange(0, 20), np.array([30, 31, 32]), np.arange(34, 40)))
+                        aind = np.concatenate((np.arange(1, 21), np.array([29]), np.arange(21, 29)))
+                        # gdata(:,[1:20,31,32,33,35:40])=station_array(:,[1:20,29,21:28]); %format is
+                        gdata[:, gind] = station_array[:, aind]
+                        # different for this station: no snow temp, only 2 radiation
+                        # statistic measurements
+                    else:  # if normal station
+                        # this vector is made from looking at C-level columns and find index of raw data that matches
+                        aind = np.concatenate(
+                            (np.arange(1, 8), np.arange(18, 31), np.arange(8, 18), np.array([43]), np.arange(31, 43)))
+                        # rawindex = [1:7,18:30,8:17,43,31:42];
+                        # gdata=station_array(:,rawindex); %put data into pseudo C-level format!
+                        gdata = np.array(station_array[:, aind])
+                    gdata[gdata == -8190] = self.no_data
+                    gdata[gdata == 2080] = self.no_data
+
+                    yr = gdata[:, 1]  # get year data
+                    jday = gdata[:, 2] + gdata[:, 3] / 24  # calculate fractional julian day
+                    datenum = yr * 1e3 + jday  # number that is ascending in time
+                    numraw = int(len(datenum))  # number of total datasets before duplicate filtering
+                    udatenum, unind = np.unique(datenum, axis=0, return_index=True)  # find only unique time stamps
+                    gdata = gdata[unind, :]  # need to reassign datenum to unique values
+                    yr = gdata[:, 1]  # get year data of unique values only
+                    jday = gdata[:, 2] + gdata[:, 3] / 24  # calculate fractional julian day of unique values
+                    datenum = yr * 1.e3 + jday  # number that is ascending in time of unique values
+                    if len(unind) < numraw:
+                        numduptime = numraw - len(unind)
+                        logger.warning("GoesCleaner: Warning: Removed " + str(numduptime) + " entries out of: " + str(
+                            numraw) + " good pts from station ID: " + str(station_id) + " Reason: duplicate time tags")
+                    tind = np.argsort(udatenum)  # find indices of a sort of unique values along time
+                    gdata = gdata[tind, :]  # crop data array to unique times
+                    jday = jday[tind]  # crop jday vector to unique times
+                    yr = yr[tind]
+                    datenum = datenum[tind]  # leave only unique and sorted datenums
+                    stnum = gdata[:, 0]  # get station number vector
+
+                    swin = self._filter_values_calibrate(gdata[:, 4], section, "swmin", "swmax", "swin",
+                                                         self.no_data, self.no_data)
+
+                    swout = self._filter_values_calibrate(gdata[:, 5], section, "swmin", "swmax", "swout",
+                                                          self.no_data, self.no_data)
+
+                    # #assign and calibrate net shortwave, negative and positive values
+                    # #have different calibration coefficients according to QC code
+                    swnet = 999 * np.ones(np.size(swout, 0))
+                    swnet[gdata[:, 6] >= 0] = gdata[gdata[:, 6] >= 0, 6] * float(self.stations_config.get(section,
+                                                                                                          "swnet_pos"))
+                    swnet[gdata[:, 6] < 0] = gdata[gdata[:, 6] < 0, 6] * float(self.stations_config.get(section,
+                                                                                                        "swnet_neg"))
+                    swnet[swnet < -(float(self.stations_config.get(section, "swmax")))] = self.no_data  # filter low
+                    swnet[swnet > float(self.stations_config.get(section, "swmax"))] = self.no_data  # filter high
+
+                    pres = gdata[:, 17] + float(self.stations_config.get(section,
+                                                                         "pressure_offset"))  # barometeric pressure
+                    pres[pres < float(self.stations_config.get(section, "pmin"))] = self.no_data  # filter low
+                    pres[pres > float(self.stations_config.get(section, "pmax"))] = self.no_data  # filter high
+                    presd = np.diff(pres)  # find difference of subsequent pressure meas
+                    hrdif = np.diff(jday) * 24.  # time diff in hrs
+                    mb_per_hr = np.absolute(np.divide(presd, hrdif, out=np.zeros_like(presd), where=hrdif != 0))
+                    pjumps = np.argwhere(mb_per_hr > 10)  # find jumps > 10mb/hr (quite unnatural)
+                    pres[pjumps + 1] = self.no_data  # eliminate these single point jumps
+
+                    s_winmax = self._filter_values_calibrate(gdata[:, 31], section, "swmin", "swmax", "swin",
+                                                             self.no_data, self.no_data)
+
+                    s_woutmax = self._filter_values_calibrate(gdata[:, 32], section, "swmin", "swmax", "swout",
+                                                              0.00, self.no_data)
+
+                    s_wnetmax = 999 * np.ones_like(s_woutmax)
+                    # net radiation max
+                    s_wnetmax[gdata[:, 33] >= 0] = gdata[gdata[:, 33] >= 0, 33] * float(
+                        self.stations_config.get(section, "swnet_pos"))
+                    s_wnetmax[gdata[:, 33] < 0] = gdata[gdata[:, 33] < 0, 33] * float(
+                        self.stations_config.get(section, "swnet_neg"))
+                    s_wnetmax[
+                        s_wnetmax < -(float(self.stations_config.get(section, "swmax")))] = self.no_data  # filter low
+                    s_wnetmax[
+                        s_wnetmax > float(self.stations_config.get(section, "swmax"))] = self.no_data  # filter high
+
+                    tc1 = self._filter_values(gdata[:, 7], section, "tcmin", "tcmax")  # thermocouple 1
+
+                    tc2 = self._filter_values(gdata[:, 8], section, "tcmin", "tcmax")  # thermocouple 2
+
+                    hmp1 = self._filter_values(gdata[:, 9], section, "hmpmin", "hmpmax")  # hmp1 temp
+
+                    hmp2 = self._filter_values(gdata[:, 10], section, "hmpmin", "hmpmax")  # hmp2 temp
+
+                    rh1 = gdata[:, 11]  # HMP relative humidity 1
+                    rh1[rh1 < float(self.stations_config.get(section, "rhmin"))] = self.no_data  # filter low
+                    rh1[rh1 > float(self.stations_config.get(section, "rhmax"))] = self.no_data  # filter high
+                    rh1[(rh1 > 100) & (
+                            rh1 < float(
+                        self.stations_config.get(section, "rhmax")))] = 100  # Assign values greater than
+                    # 100 and less than rhmax to 100
+                    rh2 = gdata[:, 12]  # HMP relative humidity 2
+                    rh2[rh2 < float(self.stations_config.get(section, "rhmin"))] = self.no_data  # filter low
+                    rh2[rh2 > float(self.stations_config.get(section, "rhmax"))] = self.no_data  # filter high
+                    rh2[(rh2 > 100) & (
+                            rh2 < float(
+                        self.stations_config.get(section, "rhmax")))] = 100  # Assign values greater than
+                    # 100 and less than rhmax to 100
+
+                    ws1 = self._filter_values(gdata[:, 13], section, "wmin", "wmax")  # wind speed 1
+
+                    ws2 = self._filter_values(gdata[:, 14], section, "wmin", "wmax")  # wind speed 2
+
+                    wd1 = self._filter_values(gdata[:, 15], section, "wdmin", "wdmax")  # wind direction 1
+
+                    wd2 = self._filter_values(gdata[:, 16], section, "wdmin", "wdmax")  # wind direction 2
+
+                    sh1 = self._filter_values(gdata[:, 18], section, "shmin", "shmax")  # height above snow 1
+
+                    sh2 = self._filter_values(gdata[:, 19], section, "shmin", "shmax")  # height above snow 2
+
+                    snow_temp10 = gdata[:,
+                                  20:30]  # 10m snow temperature (many of these are non functional or not connected)
+
+                    volts = self._filter_values(gdata[:, 30], section, "battmin", "battmax")  # battery voltage
+
+                    tc1max = self._filter_values(gdata[:, 34], section, "tcmin", "tcmax")
+
+                    tc2max = self._filter_values(gdata[:, 35], section, "tcmin", "tcmax")
+
+                    tc1min = self._filter_values(gdata[:, 36], section, "tcmin", "tcmax")
+
+                    tc2min = self._filter_values(gdata[:, 37], section, "tcmin", "tcmax")
+
+                    ws1max = gdata[:, 38]  # stats
+                    ws2max = gdata[:, 39]
+                    ws1std = gdata[:, 40]
+                    ws2std = gdata[:, 41]
+                    tref = gdata[:, 42]
+
+                    # # note this code does not currently calculate the 2 and 10 m winds
+                    # # and albedo, so these are columns 1-42 of the Level C data
+                    wdata = np.column_stack(
+                        (stnum, yr, jday, swin, swout, swnet, tc1, tc2, hmp1, hmp2, rh1, rh2, ws1, ws2, wd1,
+                         wd2, pres, sh1, sh2, snow_temp10, volts, s_winmax, s_woutmax, s_wnetmax, tc1max,
+                         tc2max, tc1min, tc2min, ws1max, ws2max, ws1std, ws2std,
+                         tref))  # assemble data into final level C standard form
+                    today = datetime.now()
+                    day_of_year = (today - datetime(today.year, 1, 1)).days + 1
+                    theyear = today.year
+                    todayjday = day_of_year + today.hour / 24  # calculate fractional julian day
+                    nowdatenum = theyear * 1e3 + todayjday
+                    wdata = wdata[datenum < nowdatenum, :]  # only take entries in the past
+                    numfuturepts = len(np.argwhere(datenum > nowdatenum))
+
+                    if numfuturepts > 0:
+                        logger.warning("GoesCleaner: Warning: Removed " + str(numfuturepts) + " entries out of: " + str(
+                            len(wdata[:, 1]) + numfuturepts) + " good pts from station ID: " + str(
+                            station_id) + " Reason: time tags in future")
+
+                    # Call write_csv function to write csv files with processed data
+                    # TODO test write_csv with convertingself.no_data value to null
+                    self.writer.write_csv(wdata, station_num, yr, jday, datenum)
+
+                    # Call write_json function to write long-term and short-term json files with processed data
+                    self.writer.write_json(wdata, station_num, self.no_data)
+
+                else:  # if no data in station_array still output empty wdata array so empty file is created
+                    wdata = np.array([])
+                    logger.warning("\t{0} Station  #{1} does not have usable data".format(self.station_type, station_num))
 
 
 class CleanerFactory(object):
