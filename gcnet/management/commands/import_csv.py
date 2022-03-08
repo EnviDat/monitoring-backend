f120ac8f002ea62a08c263d6d5645da3f51def33
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2022-03-08 16:08:25 +0100

    Implemented line cleaner

--- gcnet/management/commands/import_csv.py
+++ gcnet/management/commands/import_csv.py
@@ -13,10 +13,10 @@ from postgres_copy import CopyMapping
 from django.utils.timezone import make_aware
 from django.apps import apps
 
-# from gcnet.management.commands.importers.csv_import import CsvImporter
-# from generic.util.views_helpers import get_model_cl
-from generic.util.nead import write_nead_config
-from lwf.util.cleaners import get_lwf_meteo_line_clean, get_lwf_station_line_clean
+from gcnet.util.constants import Columns
+# from lwf.util.cleaners import get_lwf_station_line_clean
+from gcnet.management.commands.importers.helpers.import_date_helpers import gcnet_utc_datetime, gcnet_utc_timestamp, \
+    quarter_day, half_day, year_day, year_week
 
 # Setup logging
 import logging
@@ -95,22 +95,21 @@ class Command(BaseCommand):
             logger.error(f'ERROR app {app} not found')
             return
 
-        # Validate model
+        # Get model
         try:
             model_class = self.get_model_cl(app, model)
         except AttributeError as e:
             logger.error(f'ERROR model {model} not found, exception {e}')
             return
 
-        # Get parent class name
-        parent_class_name = model_class.__base__.__name__
+        # Get parent class name as string
+        parent_class = model_class.__base__.__name__
+
+        # Get input_fields
+        input_fields = self.get_input_fields(parent_class)
 
         # Get line cleaner function
-        try:
-            line_cleaner = self.get_line_cleaner(parent_class_name)
-        except Exception as e:
-            logger.error(e)
-            return
+        # line_cleaner = self.get_line_cleaner(parent_class)
 
         # Assign variables used to write csv_temporary
         csv_temporary = Path(f'{data_dir}/{model}_temporary.csv')
@@ -128,7 +127,7 @@ class Command(BaseCommand):
                 sink.write(','.join(model_fields) + '\n')
                 records_written = 0
 
-                # Iterate line by line though source file
+                # Iterate line by line though source
                 while True:
 
                     line = source.readline()
@@ -138,20 +137,21 @@ class Command(BaseCommand):
 
                     line_array = [v for v in line.strip().split(',') if len(v) > 0]
 
-                    # if len(line_array) != len(model_fields):
-                    #     error_msg = f'ERROR: line has {len(line_array)} values, header has {len(model_fields)} columns'
-                    #     logger.error(error_msg)
-                    #     raise ValueError(error_msg)
-                    #
-                    # row = {model_fields[i]: line_array[i] for i in range(len(line_array))}
-                    #
-                    # # Process row and add new computed fields
-                    # # line_clean = line_cleaner(row, date_format)
-                    #
-                    # # Make timestamp_iso value a UTC timezone aware datetime object
+                    if len(line_array) != len(input_fields):
+                        error_msg = f'ERROR: line has {len(line_array)} values, header has {len(input_fields)} columns'
+                        logger.error(error_msg)
+                        raise ValueError(error_msg)
+
+                    row = {input_fields[i]: line_array[i] for i in range(len(line_array))}
+
+                    # Process row and add new computed fields
+                    # line_clean = line_cleaner(row, date_format)
+                    line_clean = self.get_clean_line(parent_class, row)
+
+                    # Make timestamp_iso value a UTC timezone aware datetime object
                     # dt_obj = line_clean['timestamp_iso']
                     # aware_dt = make_aware(dt_obj)
-                    #
+
                     # # Check if record with identical timestamp already exists in table, otherwise write record to
                     # # temporary csv file after checking for record with duplicate timestamp
                     # try:
@@ -171,11 +171,6 @@ class Command(BaseCommand):
                     #             sink.write(
                     #                 ','.join(["{0}".format(v) for v in rows_buffer[-(1 + rows_after)].values()]) + '\n')
                     #             records_written += 1
-
-                # Write nead header configuration file if applicable
-                if nead_header:
-                    header_symbol = model_class.header_symbol
-                    write_nead_config(app, nead_header, model, parent_class_name, header_symbol)
 
         except FileNotFoundError as e:
             logger.error(f'ERROR file not found {input_file}, exception {e}')
@@ -209,25 +204,61 @@ class Command(BaseCommand):
         # if os.path.isfile(f'{data_dir}/{model}_downloaded.csv'):
         #     os.remove(f'{data_dir}/{model}_downloaded.csv')
 
-    # Check which kind of line cleaner should be used
     @staticmethod
-    def get_line_cleaner(parent_class_name):
-
-        if parent_class_name == 'LWFMeteo':
-            return get_lwf_meteo_line_clean
-
-        elif parent_class_name == 'LWFStation':
-            return get_lwf_station_line_clean
-
-        elif parent_class_name == 'Station':
-            print('TESTING')
-
-        else:
-            raise Exception(f'ERROR parent class {parent_class_name} does not exist '
-                            f'or is not specified in csv_import.py')
-
-    @staticmethod
-    # Returns model class without parent_class kwarg
+    # Returns model class
     def get_model_cl(app, model):
         package = importlib.import_module(f'{app}.models')
         return getattr(package, model)
+
+    # Returns list of input_fields for specified parent_class
+    @staticmethod
+    def get_input_fields(parent_class):
+
+        if parent_class == 'Station':
+            return ['StationID', 'Year', 'Doyd', 'SWin', 'SWout', 'NetRad', 'AirTC1', 'AirTC2', 'AirT1',
+                    'AirT2', 'RH1', 'RH2', 'WS1', 'WS2', 'WD1', 'WD2', 'press', 'Sheight1', 'Sheight2',
+                    'SnowT1', 'SnowT2', 'SnowT3', 'SnowT4', 'SnowT5', 'SnowT6', 'SnowT7', 'SnowT8', 'SnowT9',
+                    'SnowT10', 'BattVolt', 'SWinMax', 'SWoutMin', 'NetRadMax', 'AirTC1Max', 'AirTC2Max',
+                    'AirTC1Min', 'AirTC2Min', 'WS1Max', 'WS2Max', 'WS1Std', 'WS2Std', 'TempRef']
+
+        else:
+            raise Exception(f'ERROR parent class {parent_class} does not exist '
+                            f'or is not specified in "import_csv.get_input_fields"')
+
+    # Returns clean line dictionary (specified by parent_class) after processing input row
+    @staticmethod
+    def get_clean_line(parent_class, row):
+
+        if parent_class == 'Station':
+            return {Columns.TIMESTAMP_ISO.value: make_aware(gcnet_utc_datetime(row['Year'], row['Doyd'])),
+                    Columns.TIMESTAMP.value: gcnet_utc_timestamp(row['Year'], row['Doyd']),
+                    Columns.YEAR.value: row['Year'],
+                    Columns.JULIANDAY.value: row['Doyd'],
+                    Columns.QUARTERDAY.value: quarter_day(row['Doyd']),
+                    Columns.HALFDAY.value: half_day(row['Doyd']),
+                    Columns.DAY.value: year_day(row['Year'], row['Doyd']),
+                    Columns.WEEK.value: year_week(row['Year'], row['Doyd']),
+                    Columns.SWIN.value: row['SWin'], Columns.SWOUT.value: row['SWout'],
+                    Columns.NETRAD.value: row['NetRad'],
+                    Columns.AIRTEMP1.value: row['AirTC1'], Columns.AIRTEMP2.value: row['AirTC2'],
+                    Columns.AIRTEMP_CS500AIR1.value: row['AirT1'],
+                    Columns.AIRTEMP_CS500AIR2.value: row['AirT2'],
+                    Columns.RH1.value: row['RH1'], Columns.RH2.value: row['RH2'],
+                    Columns.WINDSPEED1.value: row['WS1'], Columns.WINDSPEED2.value: row['WS2'],
+                    Columns.WINDDIR1.value: row['WD1'], Columns.WINDDIR2.value: row['WD2'],
+                    Columns.PRESSURE.value: row['press'],
+                    Columns.SH1.value: row['Sheight1'], Columns.SH2.value: row['Sheight2'],
+                    Columns.BATTVOLT.value: row['BattVolt'], Columns.SWIN_MAX.value: row['SWinMax'],
+                    Columns.SWOUT_MIN.value: row['SWoutMin'], Columns.NETRAD_MAX.value: row['NetRadMax'],
+                    Columns.AIRTEMP1_MAX.value: row['AirTC1Max'], Columns.AIRTEMP2_MAX.value: row['AirTC2Max'],
+                    Columns.AIRTEMP1_MIN.value: row['AirTC1Min'], Columns.AIRTEMP2_MIN.value: row['AirTC2Min'],
+                    Columns.WINDSPEED_U1_MAX.value: row['WS1Max'],
+                    Columns.WINDSPEED_U2_MAX.value: row['WS2Max'],
+                    Columns.WINDSPEED_U1_STDEV.value: row['WS1Std'],
+                    Columns.WINDSPEED_U2_STDEV.value: row['WS2Std'],
+                    Columns.REFTEMP.value: row['TempRef']
+                    }
+
+        else:
+            raise Exception(f'ERROR parent class {parent_class} does not exist '
+                            f'or is not specified in "import_csv.get_clean_line"')
