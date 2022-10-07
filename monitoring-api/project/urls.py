ec88bdad0b1e3f719cfdf8424310d3ad0033ccc3
Rebecca Kurup Buchholz <rebecca.kurup@wsl.ch>
2022-10-07 15:20:39 +0200

    refactor: added swagger schema endpoints to project/urls.py

--- monitoring-api/project/urls.py
+++ monitoring-api/project/urls.py
@@ -19,11 +19,20 @@ Including another URLconf
 # from django.conf.urls.static import static
 from django.urls import include, path
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, \
+    SpectacularSwaggerView
+
 urlpatterns = [
-    path("api/lwf/", include("lwf.urls")),
-    path("api/lwf", include("lwf.urls")),
-    path("api/gcnet/", include("gcnet.urls")),
-    path("api/gcnet", include("gcnet.urls")),
+    path('api/lwf/', include('lwf.urls')),
+    path('api/lwf', include('lwf.urls')),
+    path('api/gcnet/', include('gcnet.urls')),
+    path('api/gcnet', include('gcnet.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'),
+         name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'),
+         name='redoc'),
+
     # # url(r'^output/(?P<path>.*)$', 'django.views.static.serve', {
     # #     'document_root': settings.OUTPUT_ROOT,
     # # }),
