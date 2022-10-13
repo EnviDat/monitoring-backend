ad95ec7fe197d6ec1cd25341eb2c787050c6980e
sam <sam.woodcock@protonmail.com>
2022-10-13 15:16:19 +0100

    fix: add SCHEMA_PATH_PREFIX data-api for drf-spectacular

--- monitoring-api/project/settings.py
+++ monitoring-api/project/settings.py
@@ -166,7 +166,7 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "Django API for WSL long-term environmental monitoring data.",
     "VERSION": app_version,
     "SERVE_INCLUDE_SCHEMA": False,
-    "SCHEMA_PATH_PREFIX": r"",
+    "SCHEMA_PATH_PREFIX": r"/data-api",
     "COMPONENT_SPLIT_REQUEST": True,
     # 'SCHEMA_PATH_PREFIX_TRIM': True,
     # 'SCHEMA_PATH_PREFIX_INSERT': 'https://www.envidat.ch/data-api',
